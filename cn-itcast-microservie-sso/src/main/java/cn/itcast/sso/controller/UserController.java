c9b7d504f7ebb4ac5dcd41e633f02062f40869fc
caozhiyang123 <1250233433@qq.com>
2019-01-15 17:26:16 +0800

    change project from jar to war

--- cn-itcast-microservie-sso/src/main/java/cn/itcast/sso/controller/UserController.java
+++ cn-itcast-microservie-sso/src/main/java/cn/itcast/sso/controller/UserController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSON;
 
@@ -54,9 +55,11 @@ public class UserController {
      * 
      * @return
      */
+    @ResponseBody
     @GetMapping(value = "/login")
-    public String toLogin() {
-        return "login";
+    public ModelAndView toLogin() {
+        ModelAndView md = new ModelAndView("/login.html");
+        return md;
     }
     
     /**
