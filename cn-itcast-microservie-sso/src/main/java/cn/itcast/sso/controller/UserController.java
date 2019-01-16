dd694adea5dff24ae436f00385bfdb4e789179d2
caozhiyang123 <1250233433@qq.com>
2019-01-16 11:51:17 +0800

    udpate jsp feature

--- cn-itcast-microservie-sso/src/main/java/cn/itcast/sso/controller/UserController.java
+++ cn-itcast-microservie-sso/src/main/java/cn/itcast/sso/controller/UserController.java
@@ -11,17 +11,15 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSON;
 
@@ -31,7 +29,8 @@ import cn.itcast.sso.pojo.User;
 import cn.itcast.sso.utils.CookieUtils;
 
 
-@RestController
+//@RestController注解相当于@ResponseBody ＋ @Controller合在一起的作用。本来应该到success.jsp页面的，则其显示success
+@Controller
 @RequestMapping("/user")
 public class UserController {
     
