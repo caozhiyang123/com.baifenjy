5d2f9e629ba041d91931184e1f0afbc75c2d7aad
caozhiyang123 <1250233433@qq.com>
2019-04-11 14:12:03 +0800

    refactor spring batch feature

--- itcst-microservice-job/src/main/java/cn/itcast/job/BatchApplication.java
+++ itcst-microservice-job/src/main/java/cn/itcast/job/BatchApplication.java
@@ -1,24 +1,11 @@
 package cn.itcast.job;
 
-import java.util.Date;
-
-import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.configuration.JobLocator;
-import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.support.MapJobRegistry;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -36,28 +23,9 @@ public class BatchApplication
     
     public static void main(String[] args) throws Exception {
         context = SpringApplication.run(BatchApplication.class, args);
-        String jobName = "messageMigrationJob";
-
-        JobRegistry jobRegistry = context.getBean(JobRegistry.class);
-        Job job = jobRegistry.getJob(jobName);
-        if(job == null){ return;}
-        JobLauncher jobLauncher = context.getBean(JobLauncher.class);
-        JobExecution jobExecution = jobLauncher.run(job, createJobParams());
-        if (!jobExecution.getExitStatus().equals(ExitStatus.COMPLETED)) {
-            throw new RuntimeException(format("%s Job execution failed.", jobName));
-        }
     }
     
     public static ConfigurableApplicationContext getContext(){
         return context;
-    }
-
-    private static String format(String string, Object jobName)
-    {
-        return string.replace("%s", jobName.toString());
-    }
-
-    private static JobParameters createJobParams() {
-        return new JobParametersBuilder().addDate("date", new Date()).toJobParameters();
     }
 }
--- itcst-microservice-job/src/main/java/cn/itcast/job/task/ScheduledTask.java
+++ itcst-microservice-job/src/main/java/cn/itcast/job/task/ScheduledTask.java
@@ -4,8 +4,18 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import cn.itcast.job.BatchApplication;
 
 @Component
 public class ScheduledTask
@@ -17,14 +27,39 @@ public class ScheduledTask
         };
     };
     
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(initialDelay=3000,fixedRate = 5000)
     public void testFixRate() {
         int count = local.get().addAndGet(1);
+        executeTask();
         if(count == 10){ System.exit(0);}
         System.out.println("每隔5秒执行一次：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     }
     
-    public void executeTask(){
-        
+    public void executeTask() {
+        try
+        {
+            ConfigurableApplicationContext context = BatchApplication.getContext();
+            String jobName = "messageMigrationJob";
+            JobRegistry jobRegistry = context.getBean(JobRegistry.class);
+            Job job = jobRegistry.getJob(jobName);
+            if(job == null){ return;}
+            JobLauncher jobLauncher = context.getBean(JobLauncher.class);
+            JobExecution jobExecution = jobLauncher.run(job, createJobParams());
+            if (!jobExecution.getExitStatus().equals(ExitStatus.COMPLETED)) {
+                throw new RuntimeException(format("%s Job execution failed.", jobName));
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    private static String format(String string, Object jobName)
+    {
+        return string.replace("%s", jobName.toString());
+    }
+
+    private static JobParameters createJobParams() {
+        return new JobParametersBuilder().addDate("date", new Date()).toJobParameters();
     }
 }
