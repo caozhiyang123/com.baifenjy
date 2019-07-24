ad0cacf8ce5c2d02896c87268eb0474fbeba9741
caozhyang123 <1250233433@qq.com>
2019-07-24 11:08:53 +0800

    package

--- cn-itcast-microservie-web/src/main/java/cn/itcast/web/WebApplication.java
+++ cn-itcast-microservie-web/src/main/java/cn/itcast/web/WebApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -15,9 +17,14 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages={"cn.itcast.web"})
-public class WebApplication
+public class WebApplication extends SpringBootServletInitializer
 {
     private static final Logger log = LoggerFactory.getLogger(WebApplication.class);
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
         
     public static  void main(String[] args){
         SpringApplication.run(WebApplication.class,args);
